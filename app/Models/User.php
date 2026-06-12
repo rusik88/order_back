28b1885df26e47946cb8100105380ffbfdca7576
rusik88 <rusik.bespalov54@gmail.com>
2026-06-12 22:47:30 +0300

    Fix

--- app/Models/User.php
+++ app/Models/User.php
@@ -27,9 +27,9 @@ class User extends Authenticatable
     ];
 
     /**
-     * The attributes that should be hidden for serialization.
      *
      * @var list<string>
+     *  The attributes that should be hidden for serialization.
      */
     protected $hidden = [
         'password',
